b1775226c3c9d70f7b900015c922dc11b628e6c2
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-08-14 23:04:12 -0300

    Inicial

---
+++ atividade01-inverter-lista.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int* inverter_lista(int* lista, int tamanho) {
+    // INVERTE A LISTA RECEBIDA.
+    int inicio = 0;
+    int fim = tamanho - 1;
+
+    while(inicio < fim) {
+        int temp = lista[inicio];
+        lista[inicio] = lista[fim];
+        lista[fim] = temp;
+        inicio++;
+        fim--;
+    }
+
+    return lista;
+}
+
+int main() {
+    int lista[] = {1, 2, 3, 4, 5};
+    int n = sizeof(lista) / sizeof(lista[0]);
+    int* lista_invertida = inverter_lista(lista, n);
+
+    // PRINTAR LISTA INVERTIDA:
+    std::cout << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cout << lista_invertida[i] << " ";
+    }
+    std::cout << std::endl;
+
+    getchar();
+
+    return 0;
+}
