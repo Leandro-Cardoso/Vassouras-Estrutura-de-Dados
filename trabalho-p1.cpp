9fb2bab5f8d2cfa3ac54e209e95eda58c9f61607
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-09-13 22:52:41 -0300

    Adição da descrição de estrutura, criação e main

--- trabalho-p1.cpp
+++ trabalho-p1.cpp
@@ -2,21 +2,28 @@
 
 using namespace std;
 
-// ESTRUTURA DO NÓ DA ARVORE:
-/*
-          NÓ
-         /  \
-        /    \
-Esquerda      Direita
-*/
+// ESTRUTURA DO NO DA ARVORE:
 struct Node {
+    /*
+    Todo no em uma arvore binaria possui um valor e dois ponteiros, um para a esquerda e outro para a direita.
+
+    Exemplo:
+              NO
+             /  \
+            /    \
+    Esquerda      Direita
+    */
     int data;
     Node *left;
     Node *right;
 };
 
-// CRIAR NOVO NÓ:
+// CRIAR NOVO NO:
 Node* newNode(int data) {
+    /*
+    A cada no que for criado é necessario instanciar a estrutura do no, atribuir o valor do dado passado a uma variavel, e atribuir para os dois ponteiros do no o valor nulo, pois o no ainda nao possui filhos.
+    No final e retornado o proprio no construido.
+    */
     Node* node = new Node();
     node->data = data;
     node->left = NULL;
@@ -24,8 +31,10 @@ Node* newNode(int data) {
     return node;
 }
 
-// INSERIR NÓ NA ARVORE:
+// INSERIR NO NA ARVORE:
 Node* insert(Node* node, int data) {
+    /*
+    */
     if (node == NULL) {
         return newNode(data);
     }
@@ -39,6 +48,8 @@ Node* insert(Node* node, int data) {
 
 // IMPRIMIR ARVORE EM ORDEM CRESCENTE:
 void sortedPrint(Node* root) {
+    /*
+    */
     if (root != NULL) {
         sortedPrint(root->left);
         cout << root->data << " ";
@@ -46,23 +57,29 @@ void sortedPrint(Node* root) {
     }
 }
 
-// FUNÇÃO PRINCIPAL:
-/*
-       10
-      /  \
-     /    \
-    /      \
-   5        15
-  / \      /  \
- /   \    /    \
-1     7 13      16
-        /
-       /
-     11
-*/
+// FUNCAO PRINCIPAL:
 int main() {
+    /*
+    Nesse exmeplo de aplicacao de arvore binaria em C++ vamos criar a seguinte arvore:
+           10
+          /  \
+         /    \
+        /      \
+       5        15
+      / \      /  \
+     /   \    /    \
+    1     7 13      16
+            /
+           /
+         11
+
+    E em seguida imprimir os valores da arvore lado a lado em ordem crescente.
+    */
+    // Definicao da raiz da arvore binaria:
     Node* root = NULL;
-    root = insert(root, 10);
+    root = insert(root, 10); // <- Ver descricao da funcao...
+
+    // Insercao dos outros valores da arvore:
     insert(root, 15);
     insert(root, 5);
     insert(root, 7);
@@ -71,8 +88,9 @@ int main() {
     insert(root, 13);
     insert(root, 11);
 
+    // Impressao no terminal dos valores da arvore lado a lado de forma crescente:
     cout << std::endl << "Arvore ordenada em lista: ";
-    sortedPrint(root);
+    sortedPrint(root); // <- Ver descricao da funcao...
     cout << std::endl;
     return 0;
 }
