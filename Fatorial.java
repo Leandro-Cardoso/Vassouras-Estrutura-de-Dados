8175f53b1cd20743aa69922cf2d56d9ef9e88a4a
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-12-08 18:26:06 -0300

    Fatorial upgrade

--- Fatorial.java
+++ Fatorial.java
@@ -1,11 +1,6 @@
 public class Fatorial {
     public static int getFatorial(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        else {
-            return n * Fatorial.getFatorial(n - 1);
-        }
+        return n < 2 ? n : n * Fatorial.getFatorial(n - 1);
     }
 
     public static void main(String[] args) {
